0161295038161c96797af4c7bf47fbb668ba341f
Gabriel Bianek <gabrielbianek151@gmail.com>
2024-09-04 15:07:41 -0300

    Added cv function

--- src/SpecificHeats.jl
+++ src/SpecificHeats.jl
@@ -1,4 +1,4 @@
-function cv(Subs::String; T = 300, Base = "MO")
+function cp(Subs::String; T = 300, Base = "MO")
 
     #T (Temperature) is a keyword argument, usually T = 300 K. 
 
@@ -17,6 +17,32 @@ function cv(Subs::String; T = 300, Base = "MO")
         elseif Base == "MA"
 
             return 1/MolarMassPropTable[Subs] * (CoefMolarCpPropTable[Subs]["a"] + CoefMolarCpPropTable[Subs]["b"]*T + CoefMolarCpPropTable[Subs]["c"]*T^2 + CoefMolarCpPropTable[Subs]["d"]*T^3)
+
+        end
+
+    end
+
+end
+
+function cv(Subs::String; T = 300, Base = "MO")
+
+    #T (Temperature) is a keyword argument, usually T = 300 K. 
+
+    #Base: MO (molar) or MA (mass)
+    
+    if Subs ∉ keys(CoefMolarCvPropTable)
+
+        error("This substance is not supported yet!")
+
+    else
+
+        if Base == "MO"
+
+            return CoefMolarCpPropTable[Subs]["a"] - NatureConst["Ru"] + CoefMolarCpPropTable[Subs]["b"]*T + CoefMolarCpPropTable[Subs]["c"]*T^2 + CoefMolarCpPropTable[Subs]["d"]*T^3
+
+        elseif Base == "MA"
+
+            return 1/MolarMassPropTable[Subs] * (CoefMolarCpPropTable[Subs]["a"] - NatureConst["Ru"] + CoefMolarCpPropTable[Subs]["b"]*T + CoefMolarCpPropTable[Subs]["c"]*T^2 + CoefMolarCpPropTable[Subs]["d"]*T^3)
 
         end
 
