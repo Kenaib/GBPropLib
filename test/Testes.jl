eea5be01ff877a6325f9b391d8041e322385f1bd
Gabriel Bianek <gabrielbianek151@gmail.com>
2024-09-04 21:23:39 -0300

    More tests

--- test/Testes.jl
+++ test/Testes.jl
@@ -3,3 +3,7 @@ using GBPropLib
 println(GBPropLib.cp("O2", T = 300, Base = "MO"))
 
 GBPropLib.cv("O2", T = 300, Base = "MO")
+
+v1 = 0.287*(17+273.15)/100
+v2 = 0.287*(57+273.15)/600
+GBPropLib.s("Air", T = 600, Tref = 100, v2 = v2, v1 = v1, Base = "MA")
