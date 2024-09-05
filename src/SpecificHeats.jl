fc57baf245d30d744289218d42d9a1e8a0a0d90f
Gabriel Bianek <gabrielbianek151@gmail.com>
2024-09-04 21:23:58 -0300

    Added entropy variation (Needs UPD)

--- src/SpecificHeats.jl
+++ src/SpecificHeats.jl
@@ -103,6 +103,42 @@ function h(Subs::String; T = 300, Tref = 0, Base = "MO")
 
 end
 
+function s(Subs::String; T = 300, v2, v1, P2, P1, Tref = 0, Base = "MO")
+
+    #v2 and v1 are specific volumes at given states 1 and 2.
+
+    PropMatrix = [v1 v2; P1 P2]
+
+    NewPropMatrix = hcat(filter(row -> !any(x -> x === nothing, row), eachrow(PropMatrix))...)
+
+    if isempty(NewPropMatrix)
+
+        error("Input pressure or specific volume of state i and i+1!")
+
+    end
+
+    #Verify which PropMatrix vector is in NewPropMatrix!
+        
+    if Subs ∉ keys(CoefMolarCpPropTable)
+
+        error("This substance is not supported yet!")
+
+    else
+
+        if Base == "MO"
+
+            return (CoefMolarCpPropTable[Subs]["a"] - NatureConst["Ru"])*log(T/Tref) + CoefMolarCpPropTable[Subs]["b"]*(T - Tref) + CoefMolarCpPropTable[Subs]["c"]/2*(T^2 - Tref^2) + CoefMolarCpPropTable[Subs]["d"]/3*(T^3 - Tref^3) + NatureConst["Ru"]*log(v2/v1)
+
+        elseif Base == "MA"
+
+            return 1/MolarMassPropTable[Subs]*((CoefMolarCpPropTable[Subs]["a"] - NatureConst["Ru"])*log(T/Tref) + CoefMolarCpPropTable[Subs]["b"]*(T - Tref) + CoefMolarCpPropTable[Subs]["c"]/2*(T^2 - Tref^2) + CoefMolarCpPropTable[Subs]["d"]/3*(T^3 - Tref^3) + NatureConst["Ru"]*log(v2/v1))
+
+        end
+
+    end
+
+end
+
 
 
 
