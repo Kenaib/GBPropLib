25bfba947555912c4014634b6b8ae829388bc31e
Gabriel Bianek <gabrielbianek151@gmail.com>
2024-09-06 18:12:37 -0300

    Examples

--- test/Testes.jl
+++ test/Testes.jl
@@ -1,9 +1,21 @@
 using GBPropLib
 
-println(GBPropLib.cp("O2", T = 300, Base = "MO"))
+#cp Air at T = 300 K, molar based:
 
-GBPropLib.cv("O2", T = 300, Base = "MO")
+println("cp_MO = ", GBPropLib.cp("Air", T = 300, Base = "MO"))
 
-v1 = 0.287*(17+273.15)/100
-v2 = 0.287*(57+273.15)/600
-GBPropLib.s("Air", T = 600, Tref = 100, v2 = v2, v1 = v1, Base = "MA")
+#cp Air at T = 300 K, mass based:
+
+println("cp_MA = ", GBPropLib.cp("Air", Base = "MA"))
+
+#Air Specific Enthalpy variation from T = 273.15 K to T = 450 K, mass based:
+
+println("Δh_MA = ", GBPropLib.Δh("Air", Tref = 273.15, Base = "MA"))
+
+#Air Specific Internal Energy variation from T = 290.15 K to T = 330.15 K, mass based:
+
+println("Δu_MA = ", GBPropLib.Δu("Air", Tref = 273.15, Base = "MA"))
+
+#Air Specific Entropy variation from (P = 100 kPa, T = 290.15 K) to (P = 600 kPa, T = 330.15 K), mass based:
+
+println("Δs_MA = ", GBPropLib.Δs("Air", T = 330.15, Tref = 290.15, P2 = 600, P1 = 100, Base = "MA"))
